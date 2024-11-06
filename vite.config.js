2d9c4d27830f542fffec26dd22abde1cb36220ec
Jason Ford <fordjason@gmail.com>
2024-11-05 16:26:55 -0800

    Wire in persistence

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    port: 5888,
+  },
+  build: {
+    target: 'esnext'
+  },
+  plugins: [
+    vue(),
+    basicSsl()
+  ]
 })
