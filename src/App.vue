9e0a786483ef01704a987648e4aa9bf505990718
Jason Ford <fordjason@gmail.com>
2024-11-11 16:04:31 -0800

    Add viewer, chooser, and customizer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Chooser from './chooser.vue'
+  import Customizer from './customizer.vue'
+  import Viewer from './viewer.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <Suspense>
-    <HelloWorld />
+    <Chooser />
+    <Customizer />
+    <Viewer />
   </Suspense>
 </template>
 
---
+++ src/viewer.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div>Viewer</div>
+</template>
