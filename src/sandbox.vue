f1999e51e37a488ace1b27af1a3ff6db279f190b
Jason Ford <fordjason@gmail.com>
2024-11-13 11:33:43 -0800

    Add translation viewer

--- src/sandbox.vue
+++ src/sandbox.vue
@@ -1,13 +1,44 @@
 <script setup>
   import { ref, reactive, watch } from 'vue'
+  const TRANSLATION_DOMAIN = 'f74e9cb3-2b53-4c85-9b0c-f1d61b032b3f.localhost:5889'
+
   const myStuff = reactive(await Agent.state('my-content'))
   const activeItem = ref(null)
   const activeItemData = ref(null)
+  const translatedActiveItemData = ref(null)
+  const translationLanguage = ref('en')
 
-  watch(() => activeItem.value, async (val) => {
-    if (!val) return
-    activeItemData.value = await Agent.state(val)
-  })
+  function applyTranslations(translations, translated) {
+    translations
+      .forEach(({ path, value }) => {
+        let ref = translated
+        const p = path.slice(1)
+        console.log(ref, p)
+        while (p.length > 1 && ref[p[0]]) ref = ref[p.shift()]
+        ref[p[0]] = value
+      })
+  }
+
+  watch(
+    () => activeItem.value,
+    async (val) => {
+      if (!val) return
+      activeItemData.value = await Agent.state(val)
+    }
+  )
+
+  watch(
+    () => [activeItemData.value, translationLanguage.value],
+    async () => {
+      const translated = JSON.parse(JSON.stringify(activeItemData.value))
+      const id = activeItem.value
+      const lang = translationLanguage.value
+      const translations = await Agent.query('translation-set', [id, lang], TRANSLATION_DOMAIN)
+      console.log('TRANSLATIONS', translations)
+      applyTranslations(translations, translated)
+      translatedActiveItemData.value = translated
+    }
+  )
 
   async function addNew() {
     const newId = await Agent.create({
@@ -50,9 +81,21 @@
   <button @click="addTranslations" :disabled="!activeItem">Add Translations</button>
   <button @click="addNew">Add New</button>
   <hr>
-  <pre>{{ activeItemData }}</pre>
+  <div class="translation-container">
+    <pre>{{ activeItemData }}</pre>
+    <pre><input v-model="translationLanguage" />
+{{  translatedActiveItemData  }}</pre>
+  </div>
 </template>
 
 <style scoped>
 .active { background: chartreuse; }
+
+.translation-container {
+  display: flex;
+}
+
+.translation-container>* {
+  flex-grow: 1;
+}
 </style>
